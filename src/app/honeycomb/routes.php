318ba9a920f9a833edac9f12508b34c5336bd0d9
Mantas Gelčius <manciusg@gmail.com>
2017-03-07 12:48:30 +0200

    Updated routes

--- src/app/honeycomb/routes.php
+++ src/app/honeycomb/routes.php
@@ -6,7 +6,8 @@ Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function ()
     Route::get('slugs', ['as' => 'admin.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminView']);
 
     Route::group(['prefix' => 'api'], function () {
-        Route::get('slugs', ['as' => 'admin.api.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listData']);
+        Route::get('slugs', ['as' => 'admin.api.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@pageData']);
+        Route::get('slugs/list', ['as' => 'admin.api.slugs.list', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@list']);
         Route::get('slugs/search', ['as' => 'admin.api.slugs.search', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@search']);
         Route::get('slugs/{id}', ['as' => 'admin.api.slugs.single', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@getSingleRecord']);
         Route::post('slugs/{id}/duplicate', ['as' => 'admin.api.slugs.duplicate', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_update'], 'uses' => 'HCSlugsController@duplicate']);
