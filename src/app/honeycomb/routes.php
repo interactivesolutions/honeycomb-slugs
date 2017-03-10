fb3a20786e005d3165e7b2b2f760075361cf998a
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-10 10:37:46 +0200

    updated routes

--- src/app/honeycomb/routes.php
+++ src/app/honeycomb/routes.php
@@ -1,6 +1,7 @@
 <?php
 
-//./packages//interactivesolutions/honeycomb-slugs/src/app/routes/routes.slugs.php
+//interactivesolutions/honeycomb-slugs/src/app/routes/routes.slugs.php
+
 
 Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function () {
     Route::get('slugs', ['as' => 'admin.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminView']);
@@ -9,19 +10,6 @@ Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function ()
         Route::get('slugs', ['as' => 'admin.api.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@pageData']);
         Route::get('slugs/list', ['as' => 'admin.api.slugs.list', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@list']);
         Route::get('slugs/search', ['as' => 'admin.api.slugs.search', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@search']);
-        Route::get('slugs/{id}', ['as' => 'admin.api.slugs.single', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@getSingleRecord']);
-        Route::post('slugs/{id}/duplicate', ['as' => 'admin.api.slugs.duplicate', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_update'], 'uses' => 'HCSlugsController@duplicate']);
-        Route::post('slugs/restore', ['as' => 'admin.api.slugs.restore', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_update'], 'uses' => 'HCSlugsController@restore']);
-        Route::post('slugs/merge', ['as' => 'admin.api.slugs.merge', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_update'], 'uses' => 'HCSlugsController@merge']);
-        Route::post('slugs', ['middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_create'], 'uses' => 'HCSlugsController@create']);
-        Route::put('slugs/{id}', ['middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_update'], 'uses' => 'HCSlugsController@update']);
-        Route::delete('slugs/{id}', ['middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_delete'], 'uses' => 'HCSlugsController@delete']);
-        Route::delete('slugs', ['middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_delete'], 'uses' => 'HCSlugsController@delete']);
-        Route::delete('slugs/{id}/force', ['as' => 'admin.api.slugs.force', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_force_delete'], 'uses' => 'HCSlugsController@forceDelete']);
-        Route::delete('slugs/force', ['as' => 'admin.api.slugs.force.multi', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_force_delete'], 'uses' => 'HCSlugsController@forceDelete']);
     });
 });
 
-Route::get('/slug', function () {
-    echo generateHCSlug('/home/movies/', 'Avatar');
-});
