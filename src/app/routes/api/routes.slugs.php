ade0092cd43f3e6940fa9a9c6f64125132080588
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-05-02 12:01:42 +0300

    Updated middleware usage for Apps in API calls

--- src/app/routes/api/routes.slugs.php
+++ src/app/routes/api/routes.slugs.php
@@ -1,12 +1,12 @@
 <?php
 
-Route::group(['prefix' => 'api', 'middleware' => ['web', 'auth-apps']], function () {
-    Route::get('slugs', ['as' => 'api.v1.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminView']);
+Route::group(['prefix' => 'api', 'middleware' => ['auth-apps']], function () {
+    Route::get('slugs', ['as' => 'api.v1.slugs', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminView']);
 
     Route::group(['prefix' => 'v1/slugs'], function () {
-        Route::get('/', ['as' => 'api.v1.api.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listPage']);
-        Route::get('list/{timestamp}', ['as' => 'api.v1.api.slugs.list.update', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listUpdate']);
-        Route::get('list', ['as' => 'api.v1.api.slugs.list', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@list']);
-        Route::get('search', ['as' => 'api.v1.api.slugs.search', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@search']);
+        Route::get('/', ['as' => 'api.v1.api.slugs', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listPage']);
+        Route::get('list/{timestamp}', ['as' => 'api.v1.api.slugs.list.update', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listUpdate']);
+        Route::get('list', ['as' => 'api.v1.api.slugs.list', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@list']);
+        Route::get('search', ['as' => 'api.v1.api.slugs.search', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@search']);
     });
 });
