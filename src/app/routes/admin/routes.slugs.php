3cb7aaed5873d4839d5b033ac9f0ed9bf30d5af7
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-05-17 13:17:48 +0300

    Updated route names

--- src/app/routes/admin/routes.slugs.php
+++ src/app/routes/admin/routes.slugs.php
@@ -1,12 +1,11 @@
 <?php
 
-Route::group(['prefix' => env('HC_ADMIN_URL'), 'middleware' => ['web', 'auth']], function () {
-    Route::get('slugs', ['as' => 'admin.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminView']);
+Route::group(['prefix' => env('HC_ADMIN_URL'), 'middleware' => ['web', 'auth']], function ()
+{
+    Route::get('slugs', ['as' => 'admin.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminIndex']);
 
-    Route::group(['prefix' => 'api/slugs'], function () {
-        Route::get('/', ['as' => 'admin.api.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listPage']);
-        Route::get('list/{timestamp}', ['as' => 'admin.api.slugs.list.update', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listUpdate']);
-        Route::get('list', ['as' => 'admin.api.slugs.list', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@list']);
-        Route::get('search', ['as' => 'admin.api.slugs.search', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@search']);
+    Route::group(['prefix' => 'api/slugs'], function ()
+    {
+        Route::get('/', ['as' => 'admin.api.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@apiIndexPaginate']);
     });
 });
--- src/app/routes/api/routes.slugs.php
+++ src/app/routes/api/routes.slugs.php
@@ -1,12 +1,17 @@
 <?php
 
-Route::group(['prefix' => 'api', 'middleware' => ['auth-apps']], function () {
-    Route::get('slugs', ['as' => 'api.v1.slugs', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminView']);
+Route::group(['prefix' => 'api', 'middleware' => ['auth-apps']], function ()
+{
+    Route::get('slugs', ['as' => 'api.v1.slugs', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminIndex']);
 
-    Route::group(['prefix' => 'v1/slugs'], function () {
-        Route::get('/', ['as' => 'api.v1.api.slugs', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listPage']);
-        Route::get('list/{timestamp}', ['as' => 'api.v1.api.slugs.list.update', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@listUpdate']);
-        Route::get('list', ['as' => 'api.v1.api.slugs.list', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@list']);
-        Route::get('search', ['as' => 'api.v1.api.slugs.search', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@search']);
+    Route::group(['prefix' => 'v1/slugs'], function ()
+    {
+        Route::get('/', ['as' => 'api.v1.slugs', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@apiIndexPaginate']);
+
+        Route::group(['prefix' => 'list'], function ()
+        {
+            Route::get('/', ['as' => 'api.v1.slugs.list', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@apiIndex']);
+            Route::get('{timestamp}', ['as' => 'api.v1.slugs.list.update', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@apiIndexSync']);
+        });
     });
 });
