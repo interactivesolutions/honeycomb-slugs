90c10900036394801666a1eb0ad0cb09c7aea404
Mantas Paulauskas <mantopastas@gmail.com>
2017-06-19 13:13:59 +0300

    updated routes

--- src/app/routes/admin/routes.slugs.php
+++ src/app/routes/admin/routes.slugs.php
@@ -2,7 +2,7 @@
 
 Route::group(['prefix' => config('hc.admin_url'), 'middleware' => ['web', 'auth']], function ()
 {
-    Route::get('slugs', ['as' => 'admin.slugs', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminIndex']);
+    Route::get('slugs', ['as' => 'admin.slugs.index', 'middleware' => ['acl:interactivesolutions_honeycomb_slugs_slugs_list'], 'uses' => 'HCSlugsController@adminIndex']);
 
     Route::group(['prefix' => 'api/slugs'], function ()
     {
